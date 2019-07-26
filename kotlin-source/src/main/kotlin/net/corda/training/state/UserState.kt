0d0a759ed9ccce8eb36e236493b9bf5a9f5baaa2
Jiratip Kiatpanya <jiratip.jk@gmail.com>
2019-07-26 11:03:49 +0700

    [ADD] UserState

---
+++ kotlin-source/src/main/kotlin/net/corda/training/state/UserState.kt
@@ -0,0 +1,20 @@
+package net.corda.training.state
+
+import net.corda.core.contracts.BelongsToContract
+import net.corda.core.contracts.LinearState
+import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.identity.Party
+import net.corda.training.contract.BondContract
+
+@BelongsToContract(BondContract::class)
+data class UserState(
+                     val name: String,
+                     val bondName: String,
+                     val owner: Party,
+                     val amount: Int,
+                     val total: Int,
+                     val unit: Int,
+                     override val linearId: UniqueIdentifier = UniqueIdentifier()): LinearState {
+
+                        override val participants: List<Party> get() = listOf(owner)
+                     }
